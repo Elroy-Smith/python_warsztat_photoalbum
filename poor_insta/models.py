d9d9e84d5b3a6757a7fb8a21755e9f952e16d552
Elroy Smith <elroysmith@test.pl>
2019-06-14 12:54:50 +0200

    Models and base.html

--- poor_insta/models.py
+++ poor_insta/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Photo(models.Model):
+    path = models.CharField(max_length=512)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
